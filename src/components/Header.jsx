7e7a5d4bc0c669fb2c7b2c7bbf0b852a7d635bc8
himchan94 <himchan9494@gmail.com>
2022-01-13 17:18:36 +0900

    feat: Created Header

---
+++ src/components/Header.jsx
@@ -0,0 +1,17 @@
+import styled from "styled-components";
+
+const Header = () => {
+  return (
+    <>
+      <HeaderBox />
+    </>
+  );
+};
+
+export default Header;
+
+const HeaderBox = styled.div`
+  width: 100%;
+  height: 1em;
+  background-color: black;
+`;
